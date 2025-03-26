5da7b869d5f86ac02d9e1ddf070804ef33314233
Stephen <stephen@wdbible.org>
2025-03-26 16:04:46 +0700

    Updated server url

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const wishingWellCommentRoutes = require("./routes/wishingWellCommentRoutes");
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? [process.env.FRONTEND_URL || " https://moodcompanion.onrender.com"]
+      ? [process.env.FRONTEND_URL || " https://mood-companion.onrender.com"]
       : ["http://localhost:5173"], // Allow your frontend dev server
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
