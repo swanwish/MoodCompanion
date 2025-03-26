a8e99f043eb58efa7ce2bd2db366c5535d160333
michellecong <cong.m@northeastern.edu>
2025-03-25 22:32:30 -0700

    fix bug of build directory

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // routes
 // app.use("/api/auth", authRoutes);
@@ -42,7 +42,7 @@ app.use("/api/wishing-well/posts", wishingWellPostRoutes);
 app.use("/api/wishing-well/comments", wishingWellCommentRoutes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 connectDB();
