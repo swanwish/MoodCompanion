ac22d863d5ca02261c9ad83fbfdc5a92ded45e34
michellecong <cong.m@northeastern.edu>
2025-03-25 21:28:57 -0700

    config build directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Navbar from './components/common/Navbar';
-import Footer from './components/common/Footer';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import JournalsPage from './pages/JournalsPage';
-import SingleJournalPage from './pages/SingleJournalPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import JournalsPage from "./pages/JournalsPage";
+import SingleJournalPage from "./pages/SingleJournalPage";
 import PostDetailPage from "./pages/PostDetailPage";
 import PostsListPage from "./pages/PostsListPage";
-import './App.css';
+
+import "./App.css";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
