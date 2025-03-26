3e681a69c7822e7bd0f948a45cb2486efed9309e
michellecong <cong.m@northeastern.edu>
2025-03-25 23:31:33 -0700

    delete logs

--- server/app.js
+++ server/app.js
@@ -41,62 +41,8 @@ app.use("/api/users", userRoutes);
 app.use("/api/wishing-well/posts", wishingWellPostRoutes);
 app.use("/api/wishing-well/comments", wishingWellCommentRoutes);
 
-const fs = require("fs");
-
-console.log("Current directory:", __dirname);
-console.log("Directory contents:", fs.readdirSync(__dirname));
-
-// 检查上一级目录
-console.log("Parent directory:", path.join(__dirname, ".."));
-try {
-  console.log(
-    "Parent directory contents:",
-    fs.readdirSync(path.join(__dirname, ".."))
-  );
-} catch (err) {
-  console.log("Error reading parent directory:", err.message);
-}
-
-// 检查client目录是否存在
-try {
-  console.log(
-    "Client directory exists:",
-    fs.existsSync(path.join(__dirname, "../client"))
-  );
-  if (fs.existsSync(path.join(__dirname, "../client"))) {
-    console.log(
-      "Client directory contents:",
-      fs.readdirSync(path.join(__dirname, "../client"))
-    );
-
-    // 检查client/build目录
-    console.log(
-      "Client build directory exists:",
-      fs.existsSync(path.join(__dirname, "../client/build"))
-    );
-    if (fs.existsSync(path.join(__dirname, "../client/build"))) {
-      console.log(
-        "Client build directory contents:",
-        fs.readdirSync(path.join(__dirname, "../client/build"))
-      );
-    }
-  }
-} catch (err) {
-  console.log("Error checking client directory:", err.message);
-}
-
 app.get("*", (req, res) => {
-  const indexPath = path.join(__dirname, "../client/build", "index.html");
-  console.log("Trying to serve:", indexPath);
-  console.log("File exists:", fs.existsSync(indexPath));
-
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res
-      .status(404)
-      .send("Frontend build not found. Please check deployment configuration.");
-  }
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 connectDB();
